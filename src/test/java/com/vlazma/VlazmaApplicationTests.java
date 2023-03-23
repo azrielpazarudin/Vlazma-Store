2973f94349e75e130eb27b586c03268a963b776c
azrielpazarudin <sapuinjukemak@gmail.com>
2023-03-24 02:30:06 +0700

    first commit

---
+++ src/test/java/com/vlazma/VlazmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlazma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlazmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
