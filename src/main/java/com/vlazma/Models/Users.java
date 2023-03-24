8e2b363c8a1c1074654ebf0170f660107ac75eaa
azrielpazarudin <sapuinjukemak@gmail.com>
2023-03-24 14:20:16 +0700

    Users Update

--- src/main/java/com/vlazma/Models/Users.java
+++ src/main/java/com/vlazma/Models/Users.java
@@ -1,5 +1,23 @@
 package com.vlazma.Models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Users {
-    
+    private int id;
+    @Column(length = 254,unique = true)
+    private String email;
+    @Column(length = 50)
+    private String password;
+    @OneToOne
+    @JoinColumn(name="role_id")
+    private Roles role;
 }
